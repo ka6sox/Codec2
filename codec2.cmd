2533d1b6df26a4549adac2c9c24a1cf57699bde5
Tom King <ka6sox@gmail.com>
2016-12-20 18:13:26 -0800

    initial import of Codec2 project

---
+++ codec2.cmd
@@ -0,0 +1,143 @@
+# Pin name action command file
+
+# Start of element CONN1
+ChangePinName(CONN1, 5, Speaker)
+ChangePinName(CONN1, 4, Mic)
+ChangePinName(CONN1, 3, Speaker)
+ChangePinName(CONN1, 2, Mic)
+ChangePinName(CONN1, 1, Common)
+
+# Start of element CONN2
+ChangePinName(CONN2, 5, Speaker)
+ChangePinName(CONN2, 4, Mic)
+ChangePinName(CONN2, 3, Speaker)
+ChangePinName(CONN2, 2, Mic)
+ChangePinName(CONN2, 1, Common)
+
+# Start of element J2
+ChangePinName(J2, 1, 1)
+ChangePinName(J2, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element C2
+ChangePinName(C2, 1, 1)
+ChangePinName(C2, 2, 2)
+
+# Start of element C1
+ChangePinName(C1, 2, 2)
+ChangePinName(C1, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 2, 2)
+ChangePinName(R7, 1, 1)
+
+# Start of element C11
+ChangePinName(C11, 1, +)
+ChangePinName(C11, 2, -)
+
+# Start of element C10
+ChangePinName(C10, 2, -)
+ChangePinName(C10, 1, +)
+
+# Start of element C9
+ChangePinName(C9, 1, +)
+ChangePinName(C9, 2, -)
+
+# Start of element C12
+ChangePinName(C12, 1, 1)
+ChangePinName(C12, 2, 2)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element C4
+ChangePinName(C4, 2, 2)
+ChangePinName(C4, 1, 1)
+
+# Start of element C3
+ChangePinName(C3, 2, 2)
+ChangePinName(C3, 1, 1)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
+
+# Start of element U1
+ChangePinName(U1, 15, AGND)
+ChangePinName(U1, 28, DGND)
+ChangePinName(U1, 27, DCVDD)
+ChangePinName(U1, 26, XTO)
+ChangePinName(U1, 25, XTI/MCLK)
+ChangePinName(U1, 24, SCLK)
+ChangePinName(U1, 23, SDIN)
+ChangePinName(U1, 22, CSB)
+ChangePinName(U1, 21, MODE)
+ChangePinName(U1, 20, LLINEIN)
+ChangePinName(U1, 19, RLINEIN)
+ChangePinName(U1, 18, MICIN)
+ChangePinName(U1, 17, MICBIAS)
+ChangePinName(U1, 16, VMID)
+ChangePinName(U1, 14, AVDD)
+ChangePinName(U1, 13, ROUT)
+ChangePinName(U1, 12, LOUT)
+ChangePinName(U1, 11, HPGND)
+ChangePinName(U1, 10, RHPOUT)
+ChangePinName(U1, 9, LHPOUT)
+ChangePinName(U1, 8, HPVDD)
+ChangePinName(U1, 7, ADCLRC)
+ChangePinName(U1, 6, ADCDAT)
+ChangePinName(U1, 5, DACLRC)
+ChangePinName(U1, 4, DACDAT)
+ChangePinName(U1, 3, BCLK)
+ChangePinName(U1, 2, CLKOUT)
+ChangePinName(U1, 1, DBVDD)
+
+# Start of element J1
+ChangePinName(J1, 11, 11)
+ChangePinName(J1, 12, 12)
+ChangePinName(J1, 6, 6)
+ChangePinName(J1, 7, 7)
+ChangePinName(J1, 8, 8)
+ChangePinName(J1, 9, 9)
+ChangePinName(J1, 10, 10)
+ChangePinName(J1, 5, 5)
+ChangePinName(J1, 1, 1)
+ChangePinName(J1, 4, 4)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element L1
+ChangePinName(L1, 1, 1)
+ChangePinName(L1, 2, 2)
---
+++ mk_bom.sh
@@ -0,0 +1,1 @@
+gnetlist -g bom codec_pg1.sch -o bom.txt
